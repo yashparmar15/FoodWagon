74311a0f696df441927e21ec07cd8991aa55e9b2
Yash Parmar <yashparmar15700@gmail.com>
2020-05-05 14:26:40 +0530

    first commit

---
+++ JS/whatsapp.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('#myButton').floatingWhatsApp({
+        phone: '917780813063',
+        popupMessage: 'Hello, how can we help you?',
+        message: "I'd like to order a pizza",
+        showPopup: true,
+        showOnIE: false,
+        headerTitle: 'Welcome to FoodWagon!',
+        headerColor: '#4FCE5D',
+        backgroundColor: '#4FCE5D',
+        buttonImage: '<img src="img/whatsapp.svg" />'
+    });
+});
+
+
